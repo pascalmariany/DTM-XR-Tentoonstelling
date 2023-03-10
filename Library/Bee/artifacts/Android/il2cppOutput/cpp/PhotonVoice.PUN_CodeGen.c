39303f4263e38195d48c3a941ed34448ee5fa279
Pascal Mariany <info@marianydesign.com>
2023-03-10 16:33:27 +0100

    first commit

---
+++ Library/Bee/artifacts/Android/il2cppOutput/cpp/PhotonVoice.PUN_CodeGen.c
@@ -0,0 +1,273 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 Photon.Voice.PUN.PhotonVoiceNetwork Photon.Voice.PUN.PhotonVoiceNetwork::get_Instance()
+extern void PhotonVoiceNetwork_get_Instance_m218F6ABA69710F632A7161C0139CA140D242863B (void);
+// 0x00000002 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::set_Instance(Photon.Voice.PUN.PhotonVoiceNetwork)
+extern void PhotonVoiceNetwork_set_Instance_mFE74EB8EB85F20E38964B95DFA4BCCAFB0CA4A81 (void);
+// 0x00000003 System.Boolean Photon.Voice.PUN.PhotonVoiceNetwork::get_UsePunAuthValues()
+extern void PhotonVoiceNetwork_get_UsePunAuthValues_mB3EEC2C29D4EB21CC60B978D978FE5A6C272C1A5 (void);
+// 0x00000004 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::set_UsePunAuthValues(System.Boolean)
+extern void PhotonVoiceNetwork_set_UsePunAuthValues_mEE15745EA6A4D3C25A5ED0AB9B2F43E7DE88162A (void);
+// 0x00000005 System.Boolean Photon.Voice.PUN.PhotonVoiceNetwork::ConnectAndJoinRoom()
+extern void PhotonVoiceNetwork_ConnectAndJoinRoom_m2114997675595C719B2140BEB7DDEEF710ACA85E (void);
+// 0x00000006 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::Disconnect()
+extern void PhotonVoiceNetwork_Disconnect_mC74501639CE7FE2677B51DB8CE84298B9ACC0EC4 (void);
+// 0x00000007 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::Awake()
+extern void PhotonVoiceNetwork_Awake_m61BBE4695EE426575A49C0A2B2D5634F0F6A0180 (void);
+// 0x00000008 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::OnEnable()
+extern void PhotonVoiceNetwork_OnEnable_m3B48338CD41C29E1946867119CC7451695359CFA (void);
+// 0x00000009 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::OnDisable()
+extern void PhotonVoiceNetwork_OnDisable_m1DF015D317D099C4D426A6993123B27778C025C3 (void);
+// 0x0000000A System.Void Photon.Voice.PUN.PhotonVoiceNetwork::OnDestroy()
+extern void PhotonVoiceNetwork_OnDestroy_m06A5435A121C34779E41F6CABA4928ABF96B2C84 (void);
+// 0x0000000B System.Void Photon.Voice.PUN.PhotonVoiceNetwork::OnPunStateChanged(Photon.Realtime.ClientState,Photon.Realtime.ClientState)
+extern void PhotonVoiceNetwork_OnPunStateChanged_m8CA6A6319D33ABF4A0DC2D962C97197EE0BC376C (void);
+// 0x0000000C System.Void Photon.Voice.PUN.PhotonVoiceNetwork::OnVoiceStateChanged(Photon.Realtime.ClientState,Photon.Realtime.ClientState)
+extern void PhotonVoiceNetwork_OnVoiceStateChanged_mCC183F1667CBF823803416EF84E164432C226DB2 (void);
+// 0x0000000D System.Void Photon.Voice.PUN.PhotonVoiceNetwork::FollowPun(Photon.Realtime.ClientState)
+extern void PhotonVoiceNetwork_FollowPun_mC1940E04921C453E63D235B3A111FE403992022E (void);
+// 0x0000000E Photon.Voice.Unity.Speaker Photon.Voice.PUN.PhotonVoiceNetwork::SimpleSpeakerFactory(System.Int32,System.Byte,System.Object)
+extern void PhotonVoiceNetwork_SimpleSpeakerFactory_m3D34D7F56BB64F1BFCE9BC8DF15DAF702DDA394A (void);
+// 0x0000000F System.String Photon.Voice.PUN.PhotonVoiceNetwork::GetVoiceRoomName()
+extern void PhotonVoiceNetwork_GetVoiceRoomName_m3C08E7AF8F0AABF38B43CA50626E76DA241B46C6 (void);
+// 0x00000010 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::ConnectOrJoin()
+extern void PhotonVoiceNetwork_ConnectOrJoin_m315F18253CA84E7EFB2F66809B5D095FAEAE2082 (void);
+// 0x00000011 System.Boolean Photon.Voice.PUN.PhotonVoiceNetwork::Connect()
+extern void PhotonVoiceNetwork_Connect_mC9A4F3D0E0CEF78A197424479564F814068ECD2C (void);
+// 0x00000012 System.Boolean Photon.Voice.PUN.PhotonVoiceNetwork::JoinRoom(System.String)
+extern void PhotonVoiceNetwork_JoinRoom_mF336984B9581EB06D65B731833DF17F104FA3564 (void);
+// 0x00000013 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::FollowPun()
+extern void PhotonVoiceNetwork_FollowPun_mC6CBB71ECE7680334C853D2C4AA45BE1D70A2683 (void);
+// 0x00000014 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::CheckLateLinking(Photon.Voice.Unity.Speaker,System.Int32)
+extern void PhotonVoiceNetwork_CheckLateLinking_m66B04069CD791A766FB8BF2496683B870B483A1F (void);
+// 0x00000015 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::.ctor()
+extern void PhotonVoiceNetwork__ctor_mC5DD85BBDD6C1AF0A93A5896DB3F938FDBCFC544 (void);
+// 0x00000016 System.Void Photon.Voice.PUN.PhotonVoiceNetwork::.cctor()
+extern void PhotonVoiceNetwork__cctor_mD0006ACDF60029E86A21EF07D614DFBD49AF71D6 (void);
+// 0x00000017 Photon.Voice.Unity.Recorder Photon.Voice.PUN.PhotonVoiceView::get_RecorderInUse()
+extern void PhotonVoiceView_get_RecorderInUse_m81ED4D96A4DD34A4890843E7C485ABA2707B3A6A (void);
+// 0x00000018 System.Void Photon.Voice.PUN.PhotonVoiceView::set_RecorderInUse(Photon.Voice.Unity.Recorder)
+extern void PhotonVoiceView_set_RecorderInUse_m5AB337A396F56EED36C91A6E3346BFD8F5207284 (void);
+// 0x00000019 Photon.Voice.Unity.Speaker Photon.Voice.PUN.PhotonVoiceView::get_SpeakerInUse()
+extern void PhotonVoiceView_get_SpeakerInUse_m2B9E01349729560A84C0CFE8C039082CBCB9B0CF (void);
+// 0x0000001A System.Void Photon.Voice.PUN.PhotonVoiceView::set_SpeakerInUse(Photon.Voice.Unity.Speaker)
+extern void PhotonVoiceView_set_SpeakerInUse_mB96442AF8DEDD8481ACF2E4F0E0E54F560017B38 (void);
+// 0x0000001B System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsSetup()
+extern void PhotonVoiceView_get_IsSetup_mBE09A269640D7A236B969F2446CCFF8BF3D896F9 (void);
+// 0x0000001C System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsSpeaker()
+extern void PhotonVoiceView_get_IsSpeaker_m908E76FA1E71BE5EE8D3CBBFD658FC1419FDE19A (void);
+// 0x0000001D System.Void Photon.Voice.PUN.PhotonVoiceView::set_IsSpeaker(System.Boolean)
+extern void PhotonVoiceView_set_IsSpeaker_m8AAE5035ACA051312DAE77C6B86224DE57C97189 (void);
+// 0x0000001E System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsSpeaking()
+extern void PhotonVoiceView_get_IsSpeaking_m8412D81FD36F7800BC5E4A1E6E32C50EE691067D (void);
+// 0x0000001F System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsRecorder()
+extern void PhotonVoiceView_get_IsRecorder_m544CEC0E31A8201E1728CC4B2714DFC13A391FB2 (void);
+// 0x00000020 System.Void Photon.Voice.PUN.PhotonVoiceView::set_IsRecorder(System.Boolean)
+extern void PhotonVoiceView_set_IsRecorder_mBD6562E716F7DD2873DD604B749FAF81CD1E1560 (void);
+// 0x00000021 System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsRecording()
+extern void PhotonVoiceView_get_IsRecording_m402F87721119C1BB8CE611ED8F9D2C2A0905CD6E (void);
+// 0x00000022 System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsSpeakerLinked()
+extern void PhotonVoiceView_get_IsSpeakerLinked_m4361D231DA86585E7B08838F2EFDBB54877359EF (void);
+// 0x00000023 System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_IsPhotonViewReady()
+extern void PhotonVoiceView_get_IsPhotonViewReady_m0CCAA088D78918F328A397A0CACC09CB8667D0D6 (void);
+// 0x00000024 System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_RequiresSpeaker()
+extern void PhotonVoiceView_get_RequiresSpeaker_m4239E4F09BAED7D47290F8FE2E871DDC2085F444 (void);
+// 0x00000025 System.Boolean Photon.Voice.PUN.PhotonVoiceView::get_RequiresRecorder()
+extern void PhotonVoiceView_get_RequiresRecorder_m0033AF2B5DEC05B82922A13D9D89543B0DED5C9F (void);
+// 0x00000026 System.Void Photon.Voice.PUN.PhotonVoiceView::Awake()
+extern void PhotonVoiceView_Awake_m4B304C1B1825E608E228F776F4CDEE3AF0D8D1C7 (void);
+// 0x00000027 System.Void Photon.Voice.PUN.PhotonVoiceView::OnEnable()
+extern void PhotonVoiceView_OnEnable_mBD5427B51342ED42E69FB119A431EE5A281F2B33 (void);
+// 0x00000028 System.Void Photon.Voice.PUN.PhotonVoiceView::Start()
+extern void PhotonVoiceView_Start_m5DB25DE8F467B73863068C2DF0D78BFD4248BB46 (void);
+// 0x00000029 System.Void Photon.Voice.PUN.PhotonVoiceView::CheckLateLinking()
+extern void PhotonVoiceView_CheckLateLinking_m445810623C07E0D7BED548112B881344EC62F474 (void);
+// 0x0000002A System.Void Photon.Voice.PUN.PhotonVoiceView::Setup()
+extern void PhotonVoiceView_Setup_m8F4721158CFC80F4D959D52A53363F8D61369575 (void);
+// 0x0000002B System.Boolean Photon.Voice.PUN.PhotonVoiceView::SetupRecorder()
+extern void PhotonVoiceView_SetupRecorder_mAFDC3068E9633B0199CA598A2A084BF310AFA17D (void);
+// 0x0000002C System.Boolean Photon.Voice.PUN.PhotonVoiceView::SetupRecorder(Photon.Voice.Unity.Recorder)
+extern void PhotonVoiceView_SetupRecorder_m50B8BC4C33AECF050B6A2B547526DED4D8381386 (void);
+// 0x0000002D System.Boolean Photon.Voice.PUN.PhotonVoiceView::SetupSpeaker()
+extern void PhotonVoiceView_SetupSpeaker_m324ADB37EF2817B85A4710FFCD6D151FBFED0766 (void);
+// 0x0000002E System.Boolean Photon.Voice.PUN.PhotonVoiceView::SetupSpeaker(Photon.Voice.Unity.Speaker)
+extern void PhotonVoiceView_SetupSpeaker_m596814AC4DB7FE8B1A616670854B6F13487C1297 (void);
+// 0x0000002F System.Void Photon.Voice.PUN.PhotonVoiceView::SetupRecorderInUse()
+extern void PhotonVoiceView_SetupRecorderInUse_m075ABB16F7FBE2A84059F33A1E28C30B92592FF4 (void);
+// 0x00000030 System.Void Photon.Voice.PUN.PhotonVoiceView::SetupSpeakerInUse()
+extern void PhotonVoiceView_SetupSpeakerInUse_m80497D08449382A6DBF1A9C02AB7CA86D0E4CE9C (void);
+// 0x00000031 System.Void Photon.Voice.PUN.PhotonVoiceView::Init()
+extern void PhotonVoiceView_Init_m56F4D4CE28201B7BA842A2A1A13273F45317263B (void);
+// 0x00000032 System.Void Photon.Voice.PUN.PhotonVoiceView::.ctor()
+extern void PhotonVoiceView__ctor_mBEDD2B31B5CA647939A68FC8BF1C221E47DF2EFC (void);
+// 0x00000033 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::Awake()
+extern void VoiceDebugScript_Awake_mD0A95C8E60D4A6175198B549D7C1B0AFFD3EE40D (void);
+// 0x00000034 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::Update()
+extern void VoiceDebugScript_Update_mC9C4FAB7A18B7DEE6F5AC853C3BB5D0770ED5111 (void);
+// 0x00000035 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::CantHearYou()
+extern void VoiceDebugScript_CantHearYou_m01ACDDF685C91D52B833741F47D32E92C60E54E8 (void);
+// 0x00000036 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::CantHearYou(System.String,System.String,System.String,Photon.Pun.PhotonMessageInfo)
+extern void VoiceDebugScript_CantHearYou_mDB6CAF40DFA8618703797A58F620B73AC0FBDD63 (void);
+// 0x00000037 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::Reply(System.String,Photon.Realtime.Player)
+extern void VoiceDebugScript_Reply_mC62DA59510C6110A9240288DD4FC39CEDA900FD0 (void);
+// 0x00000038 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::HeresWhy(System.String,Photon.Pun.PhotonMessageInfo)
+extern void VoiceDebugScript_HeresWhy_m7F555EE870ACD4FA64233C006262C9DA5B21FC33 (void);
+// 0x00000039 System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::MaxLogs()
+extern void VoiceDebugScript_MaxLogs_mC5F627CE9ED3A19418607F0FE7F3F6ACB05A1DB5 (void);
+// 0x0000003A System.Void Photon.Voice.PUN.UtilityScripts.VoiceDebugScript::.ctor()
+extern void VoiceDebugScript__ctor_m0269E26C7BDB17ADB0A007CCC8CE1053B17208A2 (void);
+static Il2CppMethodPointer s_methodPointers[58] = 
+{
+	PhotonVoiceNetwork_get_Instance_m218F6ABA69710F632A7161C0139CA140D242863B,
+	PhotonVoiceNetwork_set_Instance_mFE74EB8EB85F20E38964B95DFA4BCCAFB0CA4A81,
+	PhotonVoiceNetwork_get_UsePunAuthValues_mB3EEC2C29D4EB21CC60B978D978FE5A6C272C1A5,
+	PhotonVoiceNetwork_set_UsePunAuthValues_mEE15745EA6A4D3C25A5ED0AB9B2F43E7DE88162A,
+	PhotonVoiceNetwork_ConnectAndJoinRoom_m2114997675595C719B2140BEB7DDEEF710ACA85E,
+	PhotonVoiceNetwork_Disconnect_mC74501639CE7FE2677B51DB8CE84298B9ACC0EC4,
+	PhotonVoiceNetwork_Awake_m61BBE4695EE426575A49C0A2B2D5634F0F6A0180,
+	PhotonVoiceNetwork_OnEnable_m3B48338CD41C29E1946867119CC7451695359CFA,
+	PhotonVoiceNetwork_OnDisable_m1DF015D317D099C4D426A6993123B27778C025C3,
+	PhotonVoiceNetwork_OnDestroy_m06A5435A121C34779E41F6CABA4928ABF96B2C84,
+	PhotonVoiceNetwork_OnPunStateChanged_m8CA6A6319D33ABF4A0DC2D962C97197EE0BC376C,
+	PhotonVoiceNetwork_OnVoiceStateChanged_mCC183F1667CBF823803416EF84E164432C226DB2,
+	PhotonVoiceNetwork_FollowPun_mC1940E04921C453E63D235B3A111FE403992022E,
+	PhotonVoiceNetwork_SimpleSpeakerFactory_m3D34D7F56BB64F1BFCE9BC8DF15DAF702DDA394A,
+	PhotonVoiceNetwork_GetVoiceRoomName_m3C08E7AF8F0AABF38B43CA50626E76DA241B46C6,
+	PhotonVoiceNetwork_ConnectOrJoin_m315F18253CA84E7EFB2F66809B5D095FAEAE2082,
+	PhotonVoiceNetwork_Connect_mC9A4F3D0E0CEF78A197424479564F814068ECD2C,
+	PhotonVoiceNetwork_JoinRoom_mF336984B9581EB06D65B731833DF17F104FA3564,
+	PhotonVoiceNetwork_FollowPun_mC6CBB71ECE7680334C853D2C4AA45BE1D70A2683,
+	PhotonVoiceNetwork_CheckLateLinking_m66B04069CD791A766FB8BF2496683B870B483A1F,
+	PhotonVoiceNetwork__ctor_mC5DD85BBDD6C1AF0A93A5896DB3F938FDBCFC544,
+	PhotonVoiceNetwork__cctor_mD0006ACDF60029E86A21EF07D614DFBD49AF71D6,
+	PhotonVoiceView_get_RecorderInUse_m81ED4D96A4DD34A4890843E7C485ABA2707B3A6A,
+	PhotonVoiceView_set_RecorderInUse_m5AB337A396F56EED36C91A6E3346BFD8F5207284,
+	PhotonVoiceView_get_SpeakerInUse_m2B9E01349729560A84C0CFE8C039082CBCB9B0CF,
+	PhotonVoiceView_set_SpeakerInUse_mB96442AF8DEDD8481ACF2E4F0E0E54F560017B38,
+	PhotonVoiceView_get_IsSetup_mBE09A269640D7A236B969F2446CCFF8BF3D896F9,
+	PhotonVoiceView_get_IsSpeaker_m908E76FA1E71BE5EE8D3CBBFD658FC1419FDE19A,
+	PhotonVoiceView_set_IsSpeaker_m8AAE5035ACA051312DAE77C6B86224DE57C97189,
+	PhotonVoiceView_get_IsSpeaking_m8412D81FD36F7800BC5E4A1E6E32C50EE691067D,
+	PhotonVoiceView_get_IsRecorder_m544CEC0E31A8201E1728CC4B2714DFC13A391FB2,
+	PhotonVoiceView_set_IsRecorder_mBD6562E716F7DD2873DD604B749FAF81CD1E1560,
+	PhotonVoiceView_get_IsRecording_m402F87721119C1BB8CE611ED8F9D2C2A0905CD6E,
+	PhotonVoiceView_get_IsSpeakerLinked_m4361D231DA86585E7B08838F2EFDBB54877359EF,
+	PhotonVoiceView_get_IsPhotonViewReady_m0CCAA088D78918F328A397A0CACC09CB8667D0D6,
+	PhotonVoiceView_get_RequiresSpeaker_m4239E4F09BAED7D47290F8FE2E871DDC2085F444,
+	PhotonVoiceView_get_RequiresRecorder_m0033AF2B5DEC05B82922A13D9D89543B0DED5C9F,
+	PhotonVoiceView_Awake_m4B304C1B1825E608E228F776F4CDEE3AF0D8D1C7,
+	PhotonVoiceView_OnEnable_mBD5427B51342ED42E69FB119A431EE5A281F2B33,
+	PhotonVoiceView_Start_m5DB25DE8F467B73863068C2DF0D78BFD4248BB46,
+	PhotonVoiceView_CheckLateLinking_m445810623C07E0D7BED548112B881344EC62F474,
+	PhotonVoiceView_Setup_m8F4721158CFC80F4D959D52A53363F8D61369575,
+	PhotonVoiceView_SetupRecorder_mAFDC3068E9633B0199CA598A2A084BF310AFA17D,
+	PhotonVoiceView_SetupRecorder_m50B8BC4C33AECF050B6A2B547526DED4D8381386,
+	PhotonVoiceView_SetupSpeaker_m324ADB37EF2817B85A4710FFCD6D151FBFED0766,
+	PhotonVoiceView_SetupSpeaker_m596814AC4DB7FE8B1A616670854B6F13487C1297,
+	PhotonVoiceView_SetupRecorderInUse_m075ABB16F7FBE2A84059F33A1E28C30B92592FF4,
+	PhotonVoiceView_SetupSpeakerInUse_m80497D08449382A6DBF1A9C02AB7CA86D0E4CE9C,
+	PhotonVoiceView_Init_m56F4D4CE28201B7BA842A2A1A13273F45317263B,
+	PhotonVoiceView__ctor_mBEDD2B31B5CA647939A68FC8BF1C221E47DF2EFC,
+	VoiceDebugScript_Awake_mD0A95C8E60D4A6175198B549D7C1B0AFFD3EE40D,
+	VoiceDebugScript_Update_mC9C4FAB7A18B7DEE6F5AC853C3BB5D0770ED5111,
+	VoiceDebugScript_CantHearYou_m01ACDDF685C91D52B833741F47D32E92C60E54E8,
+	VoiceDebugScript_CantHearYou_mDB6CAF40DFA8618703797A58F620B73AC0FBDD63,
+	VoiceDebugScript_Reply_mC62DA59510C6110A9240288DD4FC39CEDA900FD0,
+	VoiceDebugScript_HeresWhy_m7F555EE870ACD4FA64233C006262C9DA5B21FC33,
+	VoiceDebugScript_MaxLogs_mC5F627CE9ED3A19418607F0FE7F3F6ACB05A1DB5,
+	VoiceDebugScript__ctor_m0269E26C7BDB17ADB0A007CCC8CE1053B17208A2,
+};
+static const int32_t s_InvokerIndices[58] = 
+{
+	8279,
+	8131,
+	5243,
+	4253,
+	5243,
+	5448,
+	5448,
+	5448,
+	5448,
+	5448,
+	2226,
+	2226,
+	4321,
+	1105,
+	8279,
+	5448,
+	5243,
+	3094,
+	5448,
+	2440,
+	5448,
+	8320,
+	5339,
+	4346,
+	5339,
+	4346,
+	5243,
+	5243,
+	4253,
+	5243,
+	5243,
+	4253,
+	5243,
+	5243,
+	5243,
+	5243,
+	5243,
+	5448,
+	5448,
+	5448,
+	5448,
+	5448,
+	5243,
+	3094,
+	5243,
+	3094,
+	5448,
+	5448,
+	5448,
+	5448,
+	5448,
+	5448,
+	5448,
+	932,
+	2445,
+	2446,
+	5448,
+	5448,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_PhotonVoice_PUN_CodeGenModule;
+const Il2CppCodeGenModule g_PhotonVoice_PUN_CodeGenModule = 
+{
+	"PhotonVoice.PUN.dll",
+	58,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
